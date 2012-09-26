8967af608a9f9a4bc2624608658e966ea4207c8e
coder90 <kirlevon@gmail.com>
2012-09-27 03:41:32 +0400

    API add/clear functionality added. Bugfixes

---
+++ README.markdown
@@ -0,0 +1,8 @@
+Ugly Carousel
+-------------
+
+Basic jQuery carousel plugin
+
+Currently supports only ul/li style carousels
+
+Has ability to add elements and clear the carousel
--- uc/jquery-uc.js
+++ uc/jquery-uc.js
@@ -4,7 +4,7 @@
 		defaults = {};
 
 	function UC( element, options ) {
-		this.element = element;
+		this.element = $(element);
 		this.options = $.extend( {}, defaults, options );
 		this._defaults = defaults;
 		this._name = pluginName;
@@ -17,7 +17,7 @@
 
 	UC.prototype.init = function () {
 		// taking existing children
-		var items = $(this.element).children();
+		var items = this.element.children();
 		this.itemCount = items.length;
 
 		// extract first child
@@ -26,7 +26,7 @@
 		// decide which width to use
 		var singleWidth = 0;
 
-		if (this.options.itemWidth != 'undefined' && isNaN(this.options.itemWidth))
+		if (this.options.itemWidth != 'undefined' && !isNaN(this.options.itemWidth))
 		{
 			singleWidth = this.options.itemWidth;
 		} else if (firstItem != null) {
@@ -38,7 +38,7 @@
 		// decide which height to use
 		var singleHeight = 0;
 
-		if (this.options.itemHeight != 'undefined' && isNaN(this.options.itemHeight))
+		if (this.options.itemHeight != 'undefined' && !isNaN(this.options.itemHeight))
 		{
 			singleHeight = this.options.itemHeight;
 		} else if (firstItem != null) {
@@ -47,12 +47,12 @@
 
 		// calc and set total width of items container
 		var totalWidth = singleWidth * items.length;
-		$(this.element).width(totalWidth);
+		this.element.width(totalWidth);
 
 		// construct the items wrapper
 		var wrapper = $('<div></div>').width(this.options.showItems * singleWidth)
 									.height(singleHeight).addClass('uc-wrapper');
-		$(this.element).addClass('uc-slider').wrap(wrapper);
+		this.element.addClass('uc-slider').wrap(wrapper);
 
 		var $this = this;
 
@@ -82,7 +82,7 @@
 		if (this.leftDisabled) return;
 		if (this.itemWidth == 0 || isNaN(this.itemWidth)) return;
 
-		var left = $(this.element).position().left;
+		var left = this.element.position().left;
 		var leftCount = Math.abs(left) / this.itemWidth;
 
 		if (this.itemCount > leftCount + this.options.showItems) {
@@ -90,7 +90,7 @@
 			this.options.prevButton.addClass('uc-lock');
 			this.options.nextButton.addClass('uc-lock');
 			$this = this;
-			$(this.element).stop(true).animate({
+			this.element.stop(true).animate({
 				left: left
 			}, 500, function() {
 				$this.options.prevButton.removeClass('uc-lock');
@@ -113,7 +113,7 @@
 		if (this.rightDisabled) return;
 		if (this.itemWidth == 0 || isNaN(this.itemWidth)) return;
 
-		var left = $(this.element).position().left;
+		var left = this.element.position().left;
 		var leftCount = Math.abs(left) / this.itemWidth;
 
 		if (leftCount > 0) {
@@ -121,7 +121,7 @@
 			this.options.prevButton.addClass('uc-lock');
 			this.options.nextButton.addClass('uc-lock');
 			$this = this;
-			$(this.element).stop(true).animate({
+			this.element.stop(true).animate({
 				left: left
 			}, 500, function() {
 				$this.options.prevButton.removeClass('uc-lock');
@@ -139,10 +139,39 @@
 		}
 	};
 
+	/**
+	 * Remove all items
+	 */
+	UC.prototype.clear = function () {
+		this.itemCount = 0;
+		this.element.empty();
+		this.element.width(0);
+		this.options.prevButton.addClass('uc-prev-disabled');
+		this.leftDisabled = true;
+		this.options.nextButton.addClass('uc-next-disabled');
+		this.rightDisabled = true;
+	};
+
+	UC.prototype.add = function (el) {
+		var li = $('<li></li>').append(el);
+		this.element.append(li);
+		var w = this.element.width();
+		w += this.itemWidth;
+		this.element.width(w);
+		this.itemCount += 1;
+		var left = this.element.position().left;
+		var leftCount = Math.abs(left) / this.itemWidth;
+		if (this.itemCount > leftCount + this.options.showItems)
+		{
+			this.options.prevButton.removeClass('uc-prev-disabled');
+			this.leftDisabled = false;
+		}
+	};
+
 	$.fn[pluginName] = function ( options ) {
 		return this.each(function () {
-			if (!$.data(this, 'plugin_' + pluginName)) {
-				$.data(this, 'plugin_' + pluginName,
+			if (!$.data(this, pluginName)) {
+				$.data(this, pluginName,
 					new UC( this, options ));
 			}
 		});
